219cfb25373f04ac444dfb3ae0c30057e4e7b684
skitterm <skitterman@esri.com>
2020-04-01 20:58:01 -0700

    Contact info displayed next to messages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import Message from "./Message";
-import "./App.css";
 
 interface State {
   messages: any[];
@@ -31,40 +30,52 @@ class App extends Component<Props, State> {
 
   render() {
     const styles = {
+      app: {
+        margin: "auto",
+        width: "800px"
+      },
+      title: {
+        textAlign: "center" as "center"
+      },
+      content: {
+        display: "flex"
+      },
       list: {
         listStyleType: "none"
       }
     };
 
     return (
-      <div className="App">
-        <h1>Messaging App</h1>
-        {this.state.rooms.map(room => {
-          return (
-            <ul key={room._id} style={styles.list}>
-              {room.memberInfo.map((memberInfo: any) => {
-                const fullName = `${memberInfo.name.first} ${memberInfo.name.last}`;
-                return (
-                  <li key={`${room._id}-${memberInfo._id}`}>{fullName}</li>
-                );
-              })}
-            </ul>
-          );
-        })}
-        <ul style={styles.list}>
-          {this.state.messages.map(message => {
-            const fullName = `${message.user.name.first} ${message.user.name.last}`;
+      <div className="App" style={styles.app}>
+        <h1 style={styles.title}>Messaging App</h1>
+        <div style={styles.content}>
+          {this.state.rooms.map(room => {
             return (
-              <Message
-                key={message.message._id}
-                name={fullName}
-                time={message.message.timeSent}
-                thumbnail={message.user.profileImageUrl}
-                contents={message.message.contents}
-              />
+              <ul key={room._id} style={styles.list}>
+                {room.memberInfo.map((memberInfo: any) => {
+                  const fullName = `${memberInfo.name.first} ${memberInfo.name.last}`;
+                  return (
+                    <li key={`${room._id}-${memberInfo._id}`}>{fullName}</li>
+                  );
+                })}
+              </ul>
             );
           })}
-        </ul>
+          <ul style={styles.list}>
+            {this.state.messages.map(message => {
+              const fullName = `${message.user.name.first} ${message.user.name.last}`;
+              return (
+                <Message
+                  key={message.message._id}
+                  name={fullName}
+                  time={message.message.timeSent}
+                  thumbnail={message.user.profileImageUrl}
+                  contents={message.message.contents}
+                />
+              );
+            })}
+          </ul>
+        </div>
       </div>
     );
   }
