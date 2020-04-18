641dbf33fe9d39c3e80e209e8a3ee169f86a8dcd
skitterm <skitterman@esri.com>
2020-04-18 10:27:20 -0700

    #14: Users displayed on switchUser page

--- client/src/pages/SwitchUser.tsx
+++ client/src/pages/SwitchUser.tsx
@@ -1,5 +1,26 @@
 import React, { Component } from "react";
+import styled from "styled-components";
+import Button from "../components/Button";
+import Avatar from "../components/Avatar";
+import variables from "../styles/variables";
 import PageWrapper from "./PageWrapper";
+
+const Container = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-column-gap: 32px;
+`;
+
+const Item = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const StyledName = styled.h4`
+  font-size: ${variables.fontSize.lg};
+  margin: 8px 0 16px;
+`;
 
 interface State {
   users: any[];
@@ -33,7 +54,23 @@ class SwitchUser extends Component<{}, State> {
   }
 
   public render() {
-    return <PageWrapper title="Switch User">Hi</PageWrapper>;
+    return (
+      <PageWrapper title="Switch User">
+        <Container>
+          {this.state.users.map((user) => {
+            const name = `${user.name.first} ${user.name.last}`;
+
+            return (
+              <Item>
+                <Avatar size="large" thumbnail={user.thumbnail} />
+                <StyledName>{name}</StyledName>
+                <Button>Select</Button>
+              </Item>
+            );
+          })}
+        </Container>
+      </PageWrapper>
+    );
   }
 }
 
